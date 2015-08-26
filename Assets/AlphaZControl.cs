759840263442cd4816257ba1a2a9062eade8c1bb
Halvor <stektpotet@gmail.com>
2015-08-26 22:35:19 +0200

    Added a few utility tool-stuffs (needs work), and a new level :hourglass:

---
+++ Assets/AlphaZControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaZControl : MonoBehaviour //All objects that could obstruct the view while building would be a child of this
+{
+	public float zLimit, alphaValue;
+
+	void Start()
+	{
+		TestZ();
+	}
+
+	void TestZ() //This could simply be called by an editor interface-button, instead of in the Start function
+	{
+		foreach(Transform c in transform)
+		{
+			if(c.localPosition.z < zLimit)
+			{
+				MeshRenderer r = c.GetComponentInChildren<MeshRenderer>();
+				if(r != null)
+				{
+					Debug.Log(c.name);
+					r.material.color = new Color(r.material.color.r, r.material.color.g, r.material.color.b, alphaValue); //This will need to be some sort of shader-stuff TODO
+				}
+			}
+		}
+	}
+}
