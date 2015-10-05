ff82cd819a508ef185e1a8acbf16eb9af4d74be8
Froff <Fridtjof_Eik@hotmail.com>
2015-10-05 17:41:10 +0200

    Made the level creation instantiate prefabs of ground instead of creating primitives. Also tried to make the creation undo-able, to no avail.

--- Assets/Editor/LevelCreator.cs
+++ Assets/Editor/LevelCreator.cs
@@ -1,68 +1,82 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelCreator : ScriptableWizard
 {
-	const float levelDepth = 24f;
+	const float levelDepth = 16f;
 	public float 
-		gapSize,
-		gapHeight,
-		gapHeightDelta;
+		gapSize = 5.0f,
+		gapHeight = 5.0f,
+		gapHeightDelta = 0.0f;
 
 	private Transform
 		level,
 		floor,
 		wall_L,
 		wall_R;
+	static private Transform groundPrefab;
 
 	[MenuItem ("Bridger/Create Level")]
 	static void CreateWizard ()
 	{
+		groundPrefab = AssetDatabase.LoadAssetAtPath<Transform>("Assets/Level Creation/Ground.prefab") as Transform;
 		ScriptableWizard.DisplayWizard<LevelCreator>("Create Level", "Create", "Apply");
 	}
 	
 	void OnWizardCreate ()
 	{
-		level = new GameObject("LEVEL").transform;
+		if (GameObject.Find ("LEVEL") == null)
+		{
+			level = new GameObject ("LEVEL").transform;
+		}
+		else
+		{
+			level = GameObject.Find ("LEVEL").transform;
+		}
 
-		floor = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+		floor = GameObject.Instantiate(groundPrefab);
 		floor.name = "FLOOR";
 		floor.parent = level;
-		DestroyImmediate(floor.GetComponent<BoxCollider>());
-		floor.gameObject.AddComponent<BoxCollider2D>();
 
-		wall_L = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+		wall_L = GameObject.Instantiate(groundPrefab);
 		wall_L.name = "WALL_L";
 		wall_L.parent = level;
-		DestroyImmediate(wall_L.GetComponent<BoxCollider>());
-		wall_L.gameObject.AddComponent<BoxCollider2D>();
 
-		wall_R = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+		wall_R = GameObject.Instantiate(groundPrefab);
 		wall_R.name = "WALL_R";
 		wall_R.parent = level;
-		DestroyImmediate(wall_R.GetComponent<BoxCollider>());
-		wall_R.gameObject.AddComponent<BoxCollider2D>();
 
 		ApplyValues();
-	}
 		
+		DeScaleJoints (floor);
+		DeScaleJoints (wall_L);
+		DeScaleJoints (wall_R);
+		
+		Undo.IncrementCurrentGroup ();
+		Undo.RegisterCreatedObjectUndo (floor, "");
+		Undo.RegisterCreatedObjectUndo (wall_L, "");
+		Undo.RegisterCreatedObjectUndo (wall_R, "");
+		Undo.SetCurrentGroupName ("level creation");
+	}
+
 	void ApplyValues()
 	{
 		floor.localScale = new Vector3(gapSize, gapHeight, levelDepth);
-		floor.localPosition = -level.up*gapHeight * 0.5f;
+		floor.localPosition = -level.up * gapHeight * 0.5f;
 
-		wall_L.localScale = new Vector3(levelDepth, gapHeight*2, levelDepth);
-		wall_L.transform.localPosition = -level.right*0.5f*(gapSize + levelDepth);
+		wall_L.localScale = new Vector3(levelDepth, gapHeight * 2, levelDepth);
+		wall_L.transform.localPosition = -level.right * 0.5f * (gapSize + levelDepth);
 
-		wall_R.localScale = new Vector3(levelDepth, gapHeight*2 + gapHeightDelta, levelDepth);
-		wall_R.transform.localPosition = level.right*0.5f*(gapSize + levelDepth) + level.up * gapHeightDelta*0.5f;
+		wall_R.localScale = new Vector3(levelDepth, gapHeight * 2 + gapHeightDelta, levelDepth);
+		wall_R.transform.localPosition = level.right * 0.5f * (gapSize + levelDepth) + level.up * gapHeightDelta * 0.5f;
 	}
 
 	void OnWizardUpdate ()
 	{
 		gapSize -= (gapSize % Grid.gridSize);
-		gapSize += ((gapSize % Grid.gridSize > Grid.gridSize*0.5f) ? -Grid.gridSize : 0);
+		gapSize += ((gapSize % Grid.gridSize > Grid.gridSize * 0.5f) ? -Grid.gridSize : 0);
 		gapHeightDelta = Mathf.Clamp(gapHeightDelta, -gapHeight + Grid.gridSize, gapHeightDelta);
 	}
 	
@@ -79,6 +93,19 @@ public class LevelCreator : ScriptableWizard
 				wall_R = level.FindChild("WALL_R");
 
 				ApplyValues();
+			}
+		}
+	}
+
+	void DeScaleJoints(Transform jointParent)
+	{
+		Vector3 inverseParentScale = new Vector3 (1.00f / jointParent.localScale.x, 1.00f / jointParent.localScale.y, 1.00f / jointParent.localScale.z);
+
+		foreach (Transform c in jointParent)
+		{
+			if (c.name == "JointPoint")
+			{
+				c.localScale = Vector3.Scale(c.localScale, inverseParentScale);
 			}
 		}
 	}
